d4562cecf6d5a0ff3d3346a3ebb7e51d93314c2b
Sylvester <sylvester.duda@gmail.com>
2019-04-16 21:47:56 -0700

    Finished Sample

--- ann.py
+++ ann.py
@@ -5,8 +5,21 @@ import pandas as pd
 
 # Importing the dataset
 dataset = pd.read_csv('Churn_Modelling.csv')
-X = dataset.iloc[:, [2, 3]].values
-y = dataset.iloc[:, 4].values
+X = dataset.iloc[:, [3, 13]].values
+y = dataset.iloc[:, 13].values
+
+# Encoding categorical data
+# Encoding the Independent Variables
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X_1 = LabelEncoder()
+X[:, 1] = labelencoder_X_1.fit_transform(X[:, 1])
+labelencoder_X_2 = LabelEncoder()
+X[:, 2] = labelencoder_X_2.fit_transform(X[:, 2])
+onehotencoder = OneHotEncoder(categorical_features = [0])
+X = onehotencoder.fit_transform(X).toarray()
+
+# Remove a dummy variable
+X = X[:, 1:]
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.cross_validation import train_test_split
@@ -18,11 +31,33 @@ sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
-# Fitting classifier to the Training set
-# Create your classifier here
+# Creating the ANN
+# Importing the packages
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+
+# Initializing model
+classifier = Sequential()
+
+# First hidden layer
+classifier.add(Dense(output_dim = 6, init = 'uniform', activation = 'relu', input_dim = 11))
+
+# Second hidden layer
+classifier.add(Dense(output_dim = 6, init = 'uniform', activation = 'relu'))
+
+# Output layer
+classifier.add(Dense(output_dim = 1, init = 'uniform', activation = 'sigmoid'))
+
+# Compiling the ANN
+classifier.compile(optimizer = 'adam', loss='binary_crossentropy', metrics= ['accuracy'])
+
+#Fitting the ANN to the training set.
+classifier.fit(X_train, y_train, batch_size=1, nb_epoch=100 )
 
 # Predicting the Test set results
 y_pred = classifier.predict(X_test)
+y_pred = (y_pred > .5)
 
 # Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix
